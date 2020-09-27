55ec722c820d637f41f238bfd4af622eb0c0c050
Manju <manjushamanpri111@gmail.com>
2020-09-27 19:18:37 +0530

    seekbar

--- app/src/main/java/com/skydrop/jenvi/song_singleton.java
+++ app/src/main/java/com/skydrop/jenvi/song_singleton.java
@@ -3,17 +3,26 @@ package com.skydrop.jenvi;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class song_singleton {
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class song_singleton extends AppCompatActivity {
     SongsList_singleton singleton = SongsList_singleton.getInstance();
     SongModel model;
     MediaPlayer mediaPlayer;
-    int pausepos;
 
+
+    int pausepos;
+    int position;
     public static song_singleton instance = new song_singleton();
     void play(Context context) {
-        int position = MainActivity.position;
+        position = MainActivity.position;
         Toast.makeText(context,"songindex"+position,Toast.LENGTH_SHORT).show();
         model = singleton.getSingslist(position);
         if(mediaPlayer == null) {
@@ -24,7 +33,8 @@ public class song_singleton {
             mediaPlayer = MediaPlayer.create(context.getApplicationContext(), Uri.parse(model.getPath()));
             mediaPlayer.seekTo(pausepos);
             mediaPlayer.start();
-        }else if(mediaPlayer.isPlaying()){
+        }
+        else if(mediaPlayer.isPlaying()){
             mediaPlayer.stop();
             mediaPlayer = MediaPlayer.create(context.getApplicationContext(), Uri.parse(model.getPath()));
             mediaPlayer.start();
@@ -37,6 +47,58 @@ public class song_singleton {
     void stop(){
         mediaPlayer.stop();
         mediaPlayer = null;
+    }
+    void seek_Bar(final SeekBar seekBar, final TextView played_duration, TextView total_duration, final android.os.Handler handler){
+        int duration = Integer.parseInt(singleton.getSingslist(position).getDuration());
+        //total_duration.setText(Integer.toString(duration));
+        total_duration.setText(formattedTime(duration));
+        seekBar.setMax(Integer.parseInt(singleton.getSingslist(position).getDuration()));
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                if(mediaPlayer != null && b)
+                mediaPlayer.seekTo(i * 1000);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if(mediaPlayer != null)
+                    mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+     song_singleton.this.runOnUiThread(new Runnable() {
+         @Override
+         public void run() {
+             if(mediaPlayer != null){
+                 int currentPos = mediaPlayer.getCurrentPosition() ;
+                 seekBar.setProgress(currentPos);
+                 played_duration.setText(formattedTime(currentPos));
+             }
+             handler.postDelayed(this,1000);
+         }
+     });
+
+    }
+    private String formattedTime(int currentPos) {
+        String total1;
+        String total2;
+        currentPos/=1000;
+        String seconds = String.valueOf(currentPos % 60);
+        String minutes = String.valueOf(currentPos / 60);
+        total1 = minutes + ":" + "0" + seconds;
+        total2 = minutes + ":" + seconds;
+        if(seconds.length() == 1)
+            return total1;
+        else
+            return total2;
     }
 
 }
