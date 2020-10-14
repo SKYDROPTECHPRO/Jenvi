802e5d52c28e7c8731106cb49bd2c9c628df7326
siddueswar <siddueshu@gmail.com>
2020-10-14 19:10:38 +0530

    testing branch

--- app/src/main/java/com/skydrop/jenvi/Singleton.java
+++ app/src/main/java/com/skydrop/jenvi/Singleton.java
@@ -5,9 +5,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.view.View;
-
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
